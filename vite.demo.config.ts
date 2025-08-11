404c10f91a330b5cb779476fad743878adff756f
Stanescu Alexandru <92219021+stanescu1892@users.noreply.github.com>
2025-08-11 15:15:13 +0300

    Trigger Vercel redeploy

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -5,6 +5,17 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: 'dist-demo'
-  }
+    outDir: 'dist-demo',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    },
+    sourcemap: false,
+    minify: 'esbuild'
+  },
+  base: './'
 })
